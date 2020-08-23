cfded832e34bd8f28e0f4c6128feba6eaf9f3ed5
Germain <katakugermain@gmail.com>
2020-08-24 00:11:28 +0100

    static_url & media_url

--- parlementaires/settings.py
+++ parlementaires/settings.py
@@ -153,8 +153,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = os.path.join(BASE_DIR, "static-root/")
-MEDIA_ROOT = os.path.join(BASE_DIR, "media-root/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 #Messages
 from django.contrib.messages import constants as messages
