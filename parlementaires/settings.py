fa8fa64e675033a864ead1163e42ed2089724f1f
Germain <katakugermain@gmail.com>
2020-08-24 00:25:21 +0100

    media-root

--- parlementaires/settings.py
+++ parlementaires/settings.py
@@ -154,7 +154,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media-root")
 
 #Messages
 from django.contrib.messages import constants as messages
