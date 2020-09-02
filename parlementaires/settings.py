ef9a464373ce2facbb6543be6d3b0d7d5c11200c
Germain <katakugermain@gmail.com>
2020-09-02 02:12:14 +0100

    settings prod

--- parlementaires/settings.py
+++ parlementaires/settings.py
@@ -167,29 +167,29 @@ MESSAGE_TAGS = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = os.path.join(BASE_DIR, "static-root")
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+# STATIC_ROOT = os.path.join(BASE_DIR, "static-root")
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media-root')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media-root')
 
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# AWS_ACCESS_KEY_ID = '4BOJX5VWKDVH3WTGDN7S'
-# AWS_SECRET_ACCESS_KEY = 'TL6bjCUTU6oh0osWpis77EfU0/f+wI6qe3sekMu7G1U'
-# AWS_STORAGE_BUCKET_NAME = 'parlementaires-spaces'
-# AWS_S3_ENDPOINT_URL = 'https://nyc3.digitaloceanspaces.com'
-# AWS_S3_OBJECT_PARAMETERS = {
-#     'CacheControl': 'max-age=86400',
-# }
-# AWS_LOCATION = 'parlementaires-static'
+AWS_ACCESS_KEY_ID = '4BOJX5VWKDVH3WTGDN7S'
+AWS_SECRET_ACCESS_KEY = 'TL6bjCUTU6oh0osWpis77EfU0/f+wI6qe3sekMu7G1U'
+AWS_STORAGE_BUCKET_NAME = 'parlementaires-spaces'
+AWS_S3_ENDPOINT_URL = 'https://nyc3.digitaloceanspaces.com'
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'parlementaires-static'
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
-# STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
